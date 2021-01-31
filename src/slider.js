2bce376295c3733e10ae6481f88746201dfc9162
Yuliia Kunitska <yuliiakun13@gmail.com>
2021-01-31 22:03:14 +0200

    added slider js

---
+++ src/slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+        $('.feedback-container').slick({
+            autoplay: true,
+            autoplaySpeed: 3500,
+            arrows: false,
+            pauseOnHover: true
+      });
+    });
